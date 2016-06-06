ab77e8bfcc0f29d0848ef9a009901376099cec12
tip-mb <mb@tip-insurance.com>
2016-06-06 20:12:02 +0200

    nu med gittest

---
+++ gittest.rb
@@ -0,0 +1,38 @@
+friends = [
+	{
+		name: "Diego",
+		status: "Online"
+	},
+	{
+		name: "Liam",
+		status: "Away"
+	},
+	{
+		name: "Gloria",
+		status: "Online"
+	},
+	{
+		name: "Charlie",
+		status: "Away"
+	}
+]
+
+friends.each do |miniVen|
+	puts "#{miniVen[:name]}"	
+end
+
+onlineHomoer = []
+friends.each do |bosseOnline|
+	if bosseOnline[:status] = "Online"
+		onlineHomoer.push bosseOnline
+	end
+end
+
+puts "onlineHomoer:"
+puts onlineHomoer
+
+allOnline = friends.map do |minVen|
+	{name: minVen[:name], status: "Online"}
+end
+puts "allOnline"
+puts allOnline
